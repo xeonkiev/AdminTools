49872a0ff1aaf793767070be12acdd1aa96c3329
Damian Le Nouaille <dam@dln.name>
2012-02-28 18:57:40 +0100

    all

--- all.sh
+++ all.sh
@@ -1,3 +1,12 @@
+rm bashrc.txt
+rm debian-cleaner.sh
+rm debian-deploy-nginx.sh
+rm gitignore.txt
+rm htaccess.fat.txt
+rm iptables-stop.sh
+rm iptables.sh
+rm nginx_on.sh
+
 wget https://raw.github.com/damln/AdminTools/master/bashrc.txt
 wget https://raw.github.com/damln/AdminTools/master/debian-cleaner.sh
 wget https://raw.github.com/damln/AdminTools/master/debian-deploy-nginx.sh
