73a1e3163ef980e7e15f89714d210eb6b76eb776
Damian Le Nouaille <dam@dln.name>
2012-05-15 11:25:11 +0200

    iptables

--- iptables.sh
+++ iptables.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+### BEGIN INIT INFO
+# Provides: Iptables
+# Required-Start: $local_fs $network
+# Required-Stop: $local_fs $remote_fs
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: firewall
+# Description: blocking shit.
+### END INIT INFO
+
 IPTABLES=/sbin/iptables
 
 # Flush :
