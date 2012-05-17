dd1f12f1b8fa62feade4b82d48d5d11743f98974
Damian Le Nouaille <dam@dln.name>
2012-05-17 15:50:08 +0200

    correct things

--- all.sh
+++ all.sh
@@ -9,17 +9,19 @@ else
 fi
 
 mkdir -p github
-cd github
+cd github/
 
 rm ./*.* 2>/dev/null
 
+$COMMANDWEB https://raw.github.com/damln/AdminTools/master/index.php
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/bashrc.txt
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/debian-cleaner.sh
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/debian-deploy-nginx.sh
+$COMMANDWEB https://raw.github.com/damln/AdminTools/master/debian-deploy-mongodb.sh
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/gitignore.txt
-$COMMANDWEB https://raw.github.com/damln/AdminTools/master/htaccess.fat.txt
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/iptables-stop.sh
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/iptables.sh
+
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/nginx_on.sh
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/nginx.conf
 $COMMANDWEB https://raw.github.com/damln/AdminTools/master/nginx_localhost.http
