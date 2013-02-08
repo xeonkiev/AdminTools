9cb4b0c7bb13a39a3541d6cfccd3b8c0423893f9
Damian Le Nouaille <dam@dln.name>
2013-02-08 11:00:02 +0100

    add package to ruby install

--- tools/debian-deploy-ruby.sh
+++ tools/debian-deploy-ruby.sh
@@ -20,11 +20,32 @@ source ~/.rvm/scripts/rvm
 source ~/.bashrc
 source ~/.profile
 
+rvm pkg install readline
+rvm pkg install iconv
 rvm pkg install zlib
+rvm pkg install openssl
+rvm pkg install autoconf
+rvm pkg install curl
+rvm pkg install gettext
+rvm pkg install glib
+rvm pkg install libxml2
+rvm pkg install libxslt
+rvm pkg install libyaml
+rvm pkg install llvm
+rvm pkg install mono
+rvm pkg install ncurses
+rvm pkg install pkgconfig
+
+rvm reload
+
 rvm install 1.9.3
 
 source ~/.rvm/scripts/rvm
 source ~/.bashrc
 source ~/.profile
 
+rvm gemset use global
+gem install bundler
 gem install bundle
+
+rvm --default use 1.9.3
